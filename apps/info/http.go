604a6fe079bbb4f3cc3b397cd9b1994d3076027d
kevintok <kevintjahjadi@gmail.com>
2013-07-13 17:49:23 +0700

    add header and spotlight

---
+++ apps/info/http.go
@@ -0,0 +1,23 @@
+package static_pages
+
+import (
+    // "appengine"
+    // "appengine/datastore"
+    // "appengine/user"
+    "html/template"
+    "net/http"
+    // "time"
+)
+
+func init() {
+  http.HandleFunc("/info/", infoHandler)
+}
+
+var infoTmpl = template.Must(template.ParseFiles("static/templates/base.html",
+  "apps/info/templates/info.html"))
+
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+  if err := infoTmpl.Execute(w, nil); err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
+}
