b23061ee579a743beebf6cf5d3969fe477326250
kevintok <kevintjahjadi@gmail.com>
2013-07-19 23:33:44 +0700

    add more complex parallax

--- static/js/script.js
+++ static/js/script.js
@@ -14,7 +14,8 @@ $(window).bind('scroll',function(e){
 
 function parallaxScroll(){
     var scrolled = $(window).scrollTop();
-    $('#parallax1').css('top',(100-(scrolled*0.25))+'px');
+    $('#parallax1').css('top',(100+Math.sin(scrolled*0.01)*100)+'px');
+    $('#parallax1').css('left',(500-Math.cos(scrolled*0.01)*100)+'px');
     $('#parallax2').css('left',(100-(scrolled*1))+'px');
     $('#parallax3').css('top',(300-(scrolled*-0.75))+'px');
 }
