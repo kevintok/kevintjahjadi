b22341db144716809c8e132ac1db8bdc0f4f6e91
kevintok <kevintjahjadi@gmail.com>
2013-07-19 23:19:27 +0700

    add parallax page

---
+++ apps/parallax/http.go
@@ -0,0 +1,23 @@
+package static_pages
+
+import (
+    // "appengine"
+    // "appengine/datastore"
+    // "appengine/user"
+    "html/template"
+    "net/http"
+    // "time"
+)
+
+func init() {
+  http.HandleFunc("/parallax/", infoHandler)
+}
+
+var infoTmpl = template.Must(template.ParseFiles("static/templates/base.html",
+  "apps/parallax/templates/parallax.html"))
+
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+  if err := infoTmpl.Execute(w, nil); err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
+}
--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,14 @@
 // $('header div').each(function(index) {
 //     $(this).css('height', width);
 // });
+
+$(window).bind('scroll',function(e){
+    parallaxScroll();
+});
+
+function parallaxScroll(){
+    var scrolled = $(window).scrollTop();
+    $('#parallax1').css('top',(100-(scrolled*0.25))+'px');
+    $('#parallax2').css('left',(100-(scrolled*1))+'px');
+    $('#parallax3').css('top',(300-(scrolled*-0.75))+'px');
+}
